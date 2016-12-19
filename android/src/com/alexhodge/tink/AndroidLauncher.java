369334261d6dcb6f15ab7b3642ea48307a4b7266
Alex <alex.hodge@gmail.com>
2016-12-19 21:03:07 +1300

    Initial commit

---
+++ android/src/com/alexhodge/tink/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.alexhodge.tink;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.alexhodge.tink.TinkGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TinkGame(), config);
+	}
+}
---
+++ desktop/src/com/alexhodge/tink/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alexhodge.tink.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alexhodge.tink.TinkGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TinkGame(), config);
+	}
+}
